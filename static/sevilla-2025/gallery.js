86cb32dcf117fd396a1514a5eb045dbfa458d570
Manuel <m.a.n.u.e.l@posteo.net>
2025-09-20 00:22:13 +0200

    ADD: svq 2025

---
+++ static/sevilla-2025/gallery.js
@@ -0,0 +1,100 @@
+export default {
+  "title": "Sevilla 2025",
+  "topics": [
+    {
+     "title": "El viaje de ida",
+     "images":
+       [
+         "photos/img01.jpg",
+         "photos/img02.jpg",
+         "photos/img03.jpg",
+         "photos/img04.jpg",
+         "photos/img05.jpg"
+       ]
+    },
+    {
+      "title": "La Habitación",
+      "images":
+        [
+           "photos/img06.jpg"
+        ]
+    },
+    {
+      "title": "La Comida",
+      "images":
+        [
+           "photos/img07.jpg",
+           "photos/img08.jpg",
+           "photos/img24.jpg"
+        ]
+    },
+    {
+      "title": "La Ciudad",
+      "images":
+        [
+           "photos/img25.jpg",
+           "photos/img26.jpg",
+           "photos/img33.jpg",
+           "photos/img37.jpg",
+           "photos/img22.jpg",
+           "photos/img10.jpg",
+           "photos/img36.jpg"
+        ]
+    },
+    {
+      "title": "Los Jardines",
+      "images":
+        [
+           "photos/img17.jpg",
+           "photos/img16.jpg",
+           "photos/img34.jpg",
+           "photos/img35.jpg",
+           "photos/img21.jpg",
+           "photos/img20.jpg",
+           "photos/img27.jpg",
+           "photos/img42.jpg"
+        ]
+    },
+    {
+      "title": "Victoria",
+      "images":
+        [
+         "photos/img09.jpg",
+         "photos/img38.jpg",
+         "photos/img39.jpg",
+         "photos/img40.jpg"
+        ]
+    },
+    {
+      "title": "Plaza de España",
+      "images":
+        [
+         "photos/img30.jpg",
+         "photos/img41.jpg",
+         "photos/img18.jpg",
+         "photos/img19.jpg",
+         "photos/img11.jpg",
+         "photos/img12.jpg",
+         "photos/img32.jpg"
+        ]
+    },
+    {
+      "title": "Cádiz",
+      "images": [
+         "photos/img13.jpg",
+         "photos/img14.jpg",
+         "photos/img15.jpg",
+         "photos/img23.jpg",
+         "photos/img31.jpg"
+      ]
+    },
+    {
+      "title": "El viaje de vuelta",
+      "images":
+        [
+         "photos/img28.jpg",
+         "photos/img29.jpg"
+        ]
+    },
+  ]
+};
