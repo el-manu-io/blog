72f472cbb58de188f3fc85321133ed1af9d2d594
Manuel <m.a.n.u.e.l@posteo.net>
2025-09-23 23:52:16 +0200

    MOD: update alinment & co

--- static/amsterdam-2019/gallery.js
+++ static/amsterdam-2019/gallery.js
@@ -5,8 +5,51 @@ export default {
      "title": "Aankomst",
      "images":
        [
-         "photos/img08.jpg"
+         "photos/img08.jpg",
+         "photos/img09.jpg",
+         "photos/img10.jpg",
+         "photos/img11.jpg",
+         "photos/img12.jpg"
        ]
+    },
+    {
+      "title": "Accommodatie",
+      "images":
+        [
+           "photos/img21.jpg",
+           "photos/img22.jpg"
+        ]
+    },
+    {
+      "title": "Het eten",
+      "images":
+        [
+           "photos/img31.jpg",
+           "photos/img32.jpg",
+           "photos/img33.jpg",
+           "photos/img34.jpg"
+        ]
+    },
+    {
+      "title": "Amsterdam",
+      "images":
+        [
+           "photos/img41.jpg",
+           "photos/img46.jpg",
+           "photos/img42.jpg",
+           "photos/img43.jpg",
+           "photos/img44.jpg",
+           "photos/img45.jpg",
+           "photos/img47.jpg"
+        ]
+    },
+    {
+      "title": "De tuinen",
+      "images":
+        [
+           "photos/img52.jpg",
+           "photos/img51.jpg"
+        ]
     },
   ]
 };
